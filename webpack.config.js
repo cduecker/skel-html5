9ee38ff7f0b55906a1f45b7f04a0b837cf5fab5b
Stan Carrico <shcarrico@gmail.com>
2014-04-14 02:07:45 -0500

    annotations

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 var webpack = require("webpack");
 
 module.exports = {
+	//cache generated modules when watching (improves refresh performance)
 	cache: true,
+	//the source path for the entry points
 	context: __dirname + "/app",
+	//"entry" point, IE the seed file for this application
 	entry: ["index.js"],
 	resolve: {
+		//setup aliases for bower modules
+		//NOTE: Bower modules do not need aliases if you require the full path past bower_components ie
+		// if i added module "foo" via bower, it would auto resolve foo/index.js if you require "foo", or you could
+		// require('foo/foo.js').
 		alias: {
 			"lodash": "lodash/dist/lodash.js",
 			"jquery": "jquery/dist/jquery.js",
@@ -12,11 +19,16 @@ module.exports = {
 			"d3": "d3/d3.js",
 			"can": "canjs/amd/can"
 		},
+		//lookup modules in the following directories
 		modulesDirectories: ["bower_components", "node_modules", "app"]
 	},
 	plugins: [
+		//rewrite module internal references looking for the source regex to the replace target
 		new webpack.ContextReplacementPlugin(/canjs[\/\\]amd/, /^$/),
+
+		//these items will be available in all modules w/o having to explicitly require them
 		new webpack.ProvidePlugin({
+			"_":"lodash",
 			$: "jquery",
 			jQuery: "jquery",
 			can : "can"
@@ -30,6 +42,7 @@ module.exports = {
 	},
 	failOnError: false, // don't report error to grunt if webpack find errors,
 	module: {
+		//some custom CanJS loaders, courtesy of https://github.com/sykopomp
 		loaders: [
 			{
 				test: /\.ejs/,
